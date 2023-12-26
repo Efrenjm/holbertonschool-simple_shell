6879ed67e3bc7c5f941f360740715e07d47ce752
Efrenjm <efrenjimenez_96@hotmail.com>
2023-12-26 10:16:04 -0800

    new test

--- cshell.c
+++ cshell.c
@@ -2,81 +2,77 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 
-#define MAX_COMMAND_LENGTH 256
+extern char **environ;
+
+#define MAX_COMMAND_LENGTH 1024
 
 void display_prompt()
 {
 	fflush(stdout);
 }
 
-int execute_command(char *command, char *envp[])
+void execute_command(const char *command)
 {
 	pid_t pid;
 	int status;
-	char **args = (char **)malloc(2 * sizeof(char *));
-	
-	if (args == NULL)
-        {
-            perror("simple_shell");
-            _exit(EXIT_FAILURE);
-        }
-	args[0] = command;
-        args[1] = NULL;
-	
+	char *args[MAX_COMMAND_LENGTH];
+	char *token;
+	int arg_count;
+
 	pid = fork();
 	if (pid == 0)
 	{
-		if (execve(command, args, envp) == -1)
+		token = strtok((char *)command, " ");
+		while (token != NULL)
 		{
-			perror("simple_shell");
-			free(args);
-			_exit(EXIT_FAILURE);
+			args[arg_count++] = token;
+			token = strtok(NULL, " ");
 		}
+		args[arg_count] = NULL;
+
+		execvp(args[0], args);
+		perror(command);
+		exit(EXIT_FAILURE);
+
 	}
 	else if (pid > 0)
 	{
 		waitpid(pid, &status, 0);
 
 		if (WIFEXITED(status))
-			return (WEXITSTATUS(status));
+			printf("\n");
 		else
-			return (1);
+			printf("Error: %s\n", command);
 	}
 	else
 	{
 		perror("simple_shell");
 		_exit(EXIT_FAILURE);
 	}
-	return (0);
 }
 
 int main()
 {
 	char command_line[MAX_COMMAND_LENGTH];
-	int status;
-	char *envp[] = {
-        "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
-        NULL
-    };
 
 	while (1)
 	{
 		display_prompt();
 
 		if (fgets(command_line, sizeof(command_line), stdin) == NULL)
+		{
+			printf("\n");
 			break;
-
+		}
 		command_line[strcspn(command_line, "\n")] = '\0';
 
 		if (strcmp(command_line, "exit") == 0)
 			break;
 
-		status = execute_command(command_line, envp);
-
-		if (status != 0)
-			printf("simple_shell: command failed with status %d\n", status);
+		execute_command(command_line);
 	}
 	return (0);
 }
